41093cbf9a9a88172870344954ccc6e939e32446
OtherBarry <charlie.wilson4@gmail.com>
2014-12-30 15:30:15 +1100

    Int! String! Var!
    Variables have been learnt.

---
+++ Temp Monitor/C# Tutorials/Microsoft Fundamentals/variables/Variables/Variables/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            int y;
+
+            x = 12;
+            y = 2 * (x + 2);
+
+            string firstName;
+            firstName = "Yolo";
+
+
+            Console.WriteLine(firstName);
+            Console.ReadLine();
+        }
+    }
+}
